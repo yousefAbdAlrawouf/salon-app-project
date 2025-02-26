a0daead8e78c728154a336a41647501d56c14362
yousefAbdAlrawouf <yousef3936@mail.com>
2025-02-26 09:05:55 +0300

    card fix UI

--- lib/features/User_App/appointments/presentation/Widgets/CustomBookingTileWidget.dart
+++ lib/features/User_App/appointments/presentation/Widgets/CustomBookingTileWidget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:salon_app/core/constants/app_colors.dart';
 import 'package:salon_app/features/User_App/appointments/presentation/manger/appoitment_cubit.dart';
 import 'package:salon_app/features/User_App/appointments/presentation/manger/appoitment_state.dart';
@@ -28,51 +29,62 @@ Card bookingCustomWidget(bool canEdit) {
                     children: [
                       SizedBox(height: 5),
                       Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
                         children: [
-                          CircleAvatar(child: Icon(Icons.image, size: 40)),
-                          SizedBox(width: 12),
+                          ClipOval(
+                              child:
+                                  Image.asset("assets/images/profilePic.png")),
+                          SizedBox(width: 22),
                           Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
                               Text("User Name",
                                   style: TextStyle(
-                                      fontWeight: FontWeight.bold,
+                                      fontWeight: FontWeight.w700,
                                       fontSize: 20)),
                               Row(
                                 children: [
-                                  Icon(Icons.location_on),
+                                  SvgPicture.asset(
+                                      "assets/icons/booking_location.svg",
+                                      colorFilter: ColorFilter.mode(
+                                          Colors.black, BlendMode.srcIn)),
                                   SizedBox(width: 4),
                                   Text("Location Name - street 23",
                                       style: TextStyle(
                                           decoration: TextDecoration.underline,
-                                          fontWeight: FontWeight.bold))
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w700))
                                 ],
                               ),
                             ],
                           ),
+                          Spacer(),
                           Column(
                             children: [
                               Row(children: [
                                 Text(
                                   "\$12.3",
                                   style: TextStyle(
-                                      color: Color.fromRGBO(166, 77, 121, 1),
+                                      color: AppColors.primarycolor,
                                       fontSize: 18,
                                       fontWeight: FontWeight.bold),
                                 ),
-                                canEdit ? Icon(Icons.edit) : SizedBox()
+                                state.canEdit
+                                    ? Icon(
+                                        Icons.edit,
+                                        color: Colors.black,
+                                      )
+                                    : SizedBox()
                               ]),
                               state.canEdit
                                   ? Card(
                                       color: AppColors.greyColor,
                                       child: Padding(
                                         padding: const EdgeInsets.all(8.0),
-                                        child: Text("OnGoing Late",
+                                        child: Text("Ongoing Late",
                                             style: TextStyle(
                                                 color: Color.fromRGBO(
                                                     166, 77, 121, 1),
-                                                fontWeight: FontWeight.bold)),
+                                                fontWeight: FontWeight.w700)),
                                       ))
                                   : SizedBox()
                             ],
@@ -82,12 +94,26 @@ Card bookingCustomWidget(bool canEdit) {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Icon(Icons.date_range),
+                          SvgPicture.asset(
+                            "assets/icons/cellender.svg",
+                            width: 16,
+                            height: 16,
+                          ),
                           SizedBox(width: 4),
-                          Text("From 30-7-2024"),
+                          Text("From 30-7-2024",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w700, fontSize: 12)),
                           SizedBox(width: 12),
-                          Icon(Icons.timer_sharp),
-                          Text("From :3:30 Am"),
+                          SvgPicture.asset(
+                            "assets/icons/timer.svg",
+                            width: 16,
+                            height: 16,
+                          ),
+                          Text(
+                            "From :08:30 Am",
+                            style: TextStyle(
+                                fontWeight: FontWeight.w700, fontSize: 12),
+                          ),
                           TextButton(
                               onPressed: () {},
                               child: Card(
@@ -99,14 +125,13 @@ Card bookingCustomWidget(bool canEdit) {
                                       Icon(
                                         Icons.brightness_1,
                                         size: 8,
-                                        color: Color.fromRGBO(166, 77, 121, 1),
+                                        color: AppColors.primarycolor,
                                       ),
                                       Text(
                                         "  In Home",
                                         style: TextStyle(
-                                            color:
-                                                Color.fromRGBO(166, 77, 121, 1),
-                                            fontWeight: FontWeight.bold),
+                                            color: AppColors.primarycolor,
+                                            fontWeight: FontWeight.w700),
                                       )
                                     ],
                                   ),
@@ -128,16 +153,40 @@ Card bookingCustomWidget(bool canEdit) {
                                           .canEditListTile();
                                     },
                                     child: Card(
+                                        color: AppColors.bookingBackGroundColor,
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(8),
+                                            side: BorderSide(
+                                                color: Color.fromRGBO(
+                                                    227, 227, 227, 1))),
                                         child: Padding(
-                                            padding: const EdgeInsets.all(8.0),
-                                            child: Text("Edit")))),
-                                TextButton(
-                                  onPressed: () {},
-                                  child: Card(
-                                    child: Padding(
-                                      padding: const EdgeInsets.all(8.0),
-                                      child: Text("Cancel"),
-                                    ),
+                                            padding: const EdgeInsets.only(
+                                                top: 4.0,
+                                                bottom: 4,
+                                                right: 8,
+                                                left: 8),
+                                            child: Text("Edit",
+                                                style: TextStyle(
+                                                    fontWeight: FontWeight.w400,
+                                                    color: Color.fromRGBO(
+                                                        102, 102, 102, 1)))))),
+                                Card(
+                                  color: AppColors.bookingBackGroundColor,
+                                  // shape: Border.all(color: Color.fromRGBO(227, 227, 227, 1),style: BorderStyle.solid),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                      side: BorderSide(
+                                          color: Color.fromRGBO(
+                                              227, 227, 227, 1))),
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        top: 4.0, bottom: 4, right: 8, left: 8),
+                                    child: Text("Cancel",
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.w400,
+                                            color: Color.fromRGBO(
+                                                102, 102, 102, 1))),
                                   ),
                                 ),
                               ],
@@ -145,21 +194,80 @@ Card bookingCustomWidget(bool canEdit) {
                           } else if (state.canEdit == true &&
                               state.canceledListTile == false) {
                             return Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
                               children: [
-                                TextButton(
-                                  onPressed: () {
-                                    context
-                                        .read<BookingCubit>()
-                                        .canceledListTile();
-                                  },
-                                  child: Text("Client Not Coming"),
+                                Card(
+                                  color: AppColors.bookingBackGroundColor,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                      side: BorderSide(
+                                          color: Color.fromRGBO(
+                                              227, 227, 227, 1))),
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      context
+                                          .read<BookingCubit>()
+                                          .canceledListTile();
+                                    },
+                                    child: Padding(
+                                        padding: const EdgeInsets.only(
+                                            top: 4.0,
+                                            bottom: 4,
+                                            right: 12,
+                                            left: 12),
+                                        child: Text(
+                                          "Client Not Coming",
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.w400,
+                                              color: Color.fromRGBO(
+                                                  102, 102, 102, 1)),
+                                        )),
+                                  ),
                                 ),
-                                TextButton(
-                                    onPressed: () {},
-                                    child: Text("On The Way")),
-                                TextButton(
-                                    onPressed: () {}, child: Text("Done")),
+                                Card(
+                                  color: AppColors.bookingBackGroundColor,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                      side: BorderSide(
+                                          color: Color.fromRGBO(
+                                              227, 227, 227, 1))),
+                                  child: GestureDetector(
+                                      onTap: () {},
+                                      child: Padding(
+                                        padding: const EdgeInsets.only(
+                                            top: 4.0,
+                                            bottom: 4,
+                                            right: 12,
+                                            left: 12),
+                                        child: Text("On The Way",
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.w400,
+                                                color: Color.fromRGBO(
+                                                    102, 102, 102, 1))),
+                                      )),
+                                ),
+                                Card(
+                                  color: AppColors.bookingBackGroundColor,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                      side: BorderSide(
+                                          color: Color.fromRGBO(
+                                              227, 227, 227, 1))),
+                                  child: GestureDetector(
+                                      onTap: () {},
+                                      child: Padding(
+                                        padding: const EdgeInsets.only(
+                                            top: 4.0,
+                                            bottom: 4,
+                                            right: 12,
+                                            left: 12),
+                                        child: Text("Done",
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.w400,
+                                                color: Color.fromRGBO(
+                                                    102, 102, 102, 1))),
+                                      )),
+                                ),
                               ],
                             );
                           } else {
@@ -176,9 +284,23 @@ Card bookingCustomWidget(bool canEdit) {
                                 Row(
                                   mainAxisAlignment: MainAxisAlignment.end,
                                   children: [
-                                    TextButton(
-                                        onPressed: () {},
-                                        child: Text("Delete")),
+                                    Card(
+                                        color: AppColors.bookingBackGroundColor,
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(8),
+                                      side: BorderSide(
+                                          color: Color.fromRGBO(
+                                              227, 227, 227, 1))),
+                                      child: GestureDetector(
+                                          onTap: () {},
+                                          child: Padding(
+                                            padding: const EdgeInsets.only(top: 4.0,bottom: 4,right: 8,left: 8),
+                                            child: Text("Delete", style: TextStyle(
+                                                  fontWeight: FontWeight.w400,
+                                                  color: Color.fromRGBO(
+                                                      102, 102, 102, 1))),
+                                          )),
+                                    ),
                                   ],
                                 ),
                               ],
