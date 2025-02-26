91f5ada80e5bdb625670030b9195c72b8b4d64ea
yousefAbdAlrawouf <yousef3936@mail.com>
2025-02-26 08:23:29 +0300

    add svg icon for bottom bar

--- lib/features/Provider_App/Home_provider/presentation/pages/home_page.dart
+++ lib/features/Provider_App/Home_provider/presentation/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:salon_app/core/constants/app_colors.dart';
 import 'package:salon_app/features/Provider_App/Home_provider/presentation/pages/BookingContent.dart';
 import 'package:salon_app/features/Provider_App/Home_provider/presentation/pages/Home_content.dart';
 import 'package:salon_app/features/Provider_App/Home_provider/presentation/pages/Profile_content.dart';
@@ -17,11 +19,11 @@ class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
 
   static final List<Widget> _widgetOptions = <Widget>[
-    HomeContent(), 
-    BookingsContent(), 
+    HomeContent(),
+    BookingsContent(),
     Showproductsandservices(),
-    OrdersContent(), 
-    ProfileContent(), 
+    OrdersContent(),
+    ProfileContent(),
   ];
 
   void _onItemTapped(int index) {
@@ -33,34 +35,66 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _widgetOptions[_selectedIndex], 
+      body: _widgetOptions[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
+        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.w700,color: Color.fromRGBO(179, 179, 179, 1)),
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.w700,color: AppColors.primarycolor),
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
-        selectedItemColor: Color.fromRGBO(166, 77, 121, 1),
-        unselectedItemColor: Colors.black,
+        selectedItemColor: AppColors.primarycolor,
+        unselectedItemColor: Color.fromRGBO(179, 179, 179, 1),
         showUnselectedLabels: true,
         type: BottomNavigationBarType.fixed,
         backgroundColor: Colors.white,
-        items: const <BottomNavigationBarItem>[
+        items: [
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
+            icon: _selectedIndex == 0
+                ? ColorFiltered(
+                    colorFilter: ColorFilter.mode(
+                        AppColors.primarycolor, BlendMode.srcIn),
+                    child: SvgPicture.asset("assets/icons/home.svg"),
+                  )
+                : SvgPicture.asset("assets/icons/home.svg"),
             label: 'Home',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.event),
+            icon: _selectedIndex == 1
+                ? ColorFiltered(
+                    colorFilter: ColorFilter.mode(
+                        AppColors.primarycolor, BlendMode.srcIn),
+                    child: SvgPicture.asset("assets/icons/booking.svg"),
+                  )
+                : SvgPicture.asset("assets/icons/booking.svg"),
             label: 'Bookings',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.add_circle_outline),
+            icon: _selectedIndex == 2
+                ? ColorFiltered(
+                    colorFilter: ColorFilter.mode(
+                        AppColors.primarycolor, BlendMode.srcIn),
+                    child: SvgPicture.asset("assets/icons/add.svg"),
+                  )
+                : SvgPicture.asset("assets/icons/add.svg"),
             label: 'Add',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.shopping_cart),
+            icon: _selectedIndex == 3
+                ? ColorFiltered(
+                    colorFilter: ColorFilter.mode(
+                        AppColors.primarycolor, BlendMode.srcIn),
+                    child: SvgPicture.asset("assets/icons/order.svg"),
+                  )
+                : SvgPicture.asset("assets/icons/order.svg"),
             label: 'Order',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person),
+            icon: _selectedIndex == 4
+                ? ColorFiltered(
+                    colorFilter: ColorFilter.mode(
+                        AppColors.primarycolor, BlendMode.srcIn),
+                    child: SvgPicture.asset("assets/icons/profile.svg"),
+                  )
+                : SvgPicture.asset("assets/icons/profile.svg"),
             label: 'Profile',
           ),
         ],
@@ -70,7 +104,7 @@ class _HomePageState extends State<HomePage> {
 }
 
 
-
+// colorFilter: ColorFilter.mode(AppColors.primarycolor, BlendMode.srcIn),
 
 
 
